d7fba9f4a48573dc26eff964ca974a3682d47714
Luyao Zhou <l4zhou@ucsd.edu>
2014-10-23 14:15:00 -0700

    Junit skeleton tester created

---
+++ CheckAccountTester.java
@@ -0,0 +1,15 @@
+/**
+ * Junit tester for CheckAccount class
+ */
+
+import java.util.*;
+import junit.framework.TestCase;
+public class CheckAccountTester extends TestCase {
+
+	public void setUp(){
+		Account chkAccount = new CheckAccount();
+	}
+
+	public void testOne(){
+	}
+}
---
+++ SavingAccountTester.java
@@ -0,0 +1,15 @@
+/**
+ * Junit tester for SavingAccount class
+ */
+
+import java.util.*;
+import junit.framework.TestCase;
+public class SavingAccountTester extends TestCase {
+
+	public void setUp(){
+		Account savAccount = new CheckAccount();
+	}
+
+	public void testOne(){
+	}
+}
